b84d5b38817c6f025dd4aab346b9ddea9a79d67c
VigneshAtmakuri <24bch188@spt.pdpu.ac.in>
2025-04-25 19:25:19 +0530

    Lab -1 Programs

---
+++ Lab 1, Program 1.py
@@ -0,0 +1,32 @@
+# Adding, Substracting, Multiplying, Dividing two numbers.
+a=int(input("Enter the first number:"))
+b=int(input("Enter the second number:"))
+print("Add =", a+b)
+print("Substract =", a-b)
+print("Multiply =", a*b)
+print("Divide =", a/b)
+
+#Converting hours into minutes
+h=int(input("Enter the number of hours:"))
+print("Min =", h*60)
+
+#Converting Dollar into Rs. and vice versa
+D=int(input("Enter the dollar:"))
+print("Dollar =", D*48)
+R=int(input("Enter the Rs.:"))
+print("Rs. =", R*48)
+
+#Convert Celcius into Fahrenheit and vice versa
+C=int(input("Enter the Temperature:"))
+print("Farenheit =", (9/5 * C) + 32)
+F=int(input("Enter the Temperature:"))
+print("Celcius =", 5/9 * (F – 32))
+
+#Area of Triangle
+H=int(input("Enter the height:"))
+L=int(input("Enter the length:"))
+print("Area of the Triangle: ", H*L/2)
+
+#Area of Circle
+R=int(input("Enter the Radius:"))
+print("Area of the circle: ",22/7 * R * R)
